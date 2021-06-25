19b0879dd75596c54bf8aa0ee0b7f3095a9f6f89
pampanaharish <harishasry@gmail.com>
2021-06-25 21:44:25 +0300

    3d commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import Todo from "./components/Todo";
+
+
+
 function App() {
   return (
-<div>Hello!</div>
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='learn react'/>
+      <Todo text='master react'/>
+      <Todo text='full react'/>
+    
+    </div>
   );
 }
 
